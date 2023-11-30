7ab684340ce5b30c4bf66289ff239c2d791b8f20
Richiisontheway <mineiriccardo@gmail.com>
2023-11-30 15:13:37 +0100

    general fix

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,19 @@
 // Prompt Nome
 
 let nome = prompt ('Dimmi il tuo nome');
-document.writeln(nome)
+//document.writeln(nome)
 
 // Prompt Cognome
 
 let cognome = prompt ('Dimmi il tuo Cognome');
-document.writeln(cognome)
+// document.writeln(cognome)
 
 // Prompt Colore Preferito
 
 let colore = prompt ('Dimmi il tuo colore preferito');
-document.writeln(colore)
-document.writeln(114)
+
+
+//114
+
+let psw = (nome + cognome + colore + 114)
+document.writeln (psw)
