38fbc300bf9330985aeeccfe2080bf9ca5c92325
Richiisontheway <mineiriccardo@gmail.com>
2023-11-30 15:04:14 +0100

    esercizio finito(in teoria)

---
+++ js/scripts.js
@@ -0,0 +1,15 @@
+// Prompt Nome
+
+let nome = prompt ('Dimmi il tuo nome');
+document.writeln(nome)
+
+// Prompt Cognome
+
+let cognome = prompt ('Dimmi il tuo Cognome');
+document.writeln(cognome)
+
+// Prompt Colore Preferito
+
+let colore = prompt ('Dimmi il tuo colore preferito');
+document.writeln(colore)
+document.writeln(114)
